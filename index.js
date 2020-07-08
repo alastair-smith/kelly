e88a85e83f2a8a770db2547dbb5e166e00e74970
Alastair Smith <al.smith.dev@gmail.com>
2020-07-08 20:56:31 +0100

    some tidying up of gif functionality, but not gonna lie it is still a bit of a mess

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 const { createCanvas, loadImage } = require('canvas')
 const { exec } = require('child_process')
 const GifEncoder = require('gif-encoder')
+
+const GIF_DIMENSIONS = [480, 360]
+const IMAGE_DIMENSIONS = [968, 681]
+const INCREASED_MAX_BUFFER = 1024 * 1024 * 5
 const MAX_WIDTH = 580
+const TEXT_COLOR = '#0049af'
+const TEXT_FONT = '38px AbhayaLibre-Regular'
+const TEXT_POSITION = [320, -110]
+const TEXT_ROTATION = 0.6
 
 exports.handler = async event => {
   if (event.queryStringParameters && event.queryStringParameters.text) {
     let text = event.queryStringParameters.text
 
-    const canvas = createCanvas(968, 681)
+    const canvas = createCanvas(...IMAGE_DIMENSIONS)
     const context = canvas.getContext('2d')
 
-    context.font = '38px AbhayaLibre-Regular'
+    context.font = TEXT_FONT
 
     const image = await loadImage('./base-image2.jpg')
     context.drawImage(image, 0, 0)
     context.save()
-    context.rotate(0.6)
-    context.fillStyle = '#0049af'
+    context.rotate(TEXT_ROTATION)
+    context.fillStyle = TEXT_COLOR
     while (context.measureText(text).width > MAX_WIDTH) {
+      // text width is unpredictable but needs to be cropped
       text = text.substring(0, text.length - 1)
     }
-    context.fillText(text, 320, -110)
+    context.fillText(text, ...TEXT_POSITION)
     context.restore()
 
-    if (event.queryStringParameters.gif === 'yes') {
+    if (event.queryStringParameters.gif) {
         const completeGif = await new Promise((resolve, reject) => {
-          context.drawImage(canvas, 0, 0, 480, 360)
+          context.drawImage(canvas, 0, 0, ...GIF_DIMENSIONS)
 
-          const gif = new GifEncoder(480, 360)
+          const gif = new GifEncoder(...GIF_DIMENSIONS)
           const file = require('fs').createWriteStream('/tmp/img.gif')
           file.on('error', error => reject(error))
           file.on('finish', () => {
-            exec('/opt/lib/gifsicle -d 200 /tmp/img.gif -o /tmp/del.gif', (error, stdout, stderr) => {
-              if (error) return reject(error)
-              exec('/opt/lib/gifsicle --colors 256 --merge kelly1del.gif /tmp/del.gif kelly2del.gif | base64', { maxBuffer: 1024 * 1024 * 5 }, (error2, stdout2, stderr2) => {
-                if (error2) return reject(error2)
-                return resolve(stdout2)
-              })
-            })
+            // add delay to middle frame
+            exec('/opt/lib/gifsicle -d 200 /tmp/img.gif -o /tmp/del.gif', (error, stdout, stderr) => error
+              ? reject(error)
+              : exec(
+                '/opt/lib/gifsicle --colors 256 --merge kelly1del.gif /tmp/del.gif kelly2del.gif | base64',
+                { maxBuffer: INCREASED_MAX_BUFFER },
+                (error2, stdout2, stderr2) => error2 ? reject(error2): resolve(stdout2)
+              )
+            )
           })
-          const pixels = context.getImageData(0, 0, 480, 360).data
+          const pixels = context.getImageData(0, 0, ...GIF_DIMENSIONS).data
 
           gif.pipe(file)
     
@@ -79,7 +90,3 @@ exports.handler = async event => {
       }
   }
 }
-
-exports.handler({
-  queryStringParameters: { text: 'test', gif: 'yes' }
-}).then(res => console.log(res.body))
