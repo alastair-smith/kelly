c713fbc3f10db7ca3637dbd0457d162a94f286db
Alastair Smith <al.smith.dev@gmail.com>
2020-07-08 20:38:12 +0100

    working gif support but with dodgy code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { createCanvas, loadImage } = require('canvas')
+const { exec } = require('child_process')
+const GifEncoder = require('gif-encoder')
 const MAX_WIDTH = 580
 
 exports.handler = async event => {
@@ -16,20 +18,53 @@ exports.handler = async event => {
     context.rotate(0.6)
     context.fillStyle = '#0049af'
     while (context.measureText(text).width > MAX_WIDTH) {
-      console.log('text', text)
       text = text.substring(0, text.length - 1)
     }
     context.fillText(text, 320, -110)
     context.restore()
-    const buffer = canvas.toBuffer()
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'image/png'
-      },
-      body: buffer.toString('base64'),
-      isBase64Encoded: true
+    if (event.queryStringParameters.gif === 'yes') {
+        const completeGif = await new Promise((resolve, reject) => {
+          context.drawImage(canvas, 0, 0, 480, 360)
+
+          const gif = new GifEncoder(480, 360)
+          const file = require('fs').createWriteStream('/tmp/img.gif')
+          file.on('error', error => reject(error))
+          file.on('finish', () => {
+            exec('/opt/lib/gifsicle -d 200 /tmp/img.gif -o /tmp/del.gif', (error, stdout, stderr) => {
+              if (error) return reject(error)
+              exec('/opt/lib/gifsicle --colors 256 --merge kelly1del.gif /tmp/del.gif kelly2del.gif | base64', { maxBuffer: 1024 * 1024 * 5 }, (error2, stdout2, stderr2) => {
+                if (error2) return reject(error2)
+                return resolve(stdout2)
+              })
+            })
+          })
+          const pixels = context.getImageData(0, 0, 480, 360).data
+
+          gif.pipe(file)
+    
+          // Write out the image into memory
+          gif.writeHeader()
+          gif.addFrame(pixels)
+          gif.finish() 
+        })  
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'image/gif'
+          },
+          body: completeGif,
+          isBase64Encoded: true
+       }
+    } else  {
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'image/png'
+        },
+        body: canvas.toBuffer().toString('base64'),
+        isBase64Encoded: true
+     }
     }
   }
   else {
@@ -44,3 +79,7 @@ exports.handler = async event => {
       }
   }
 }
+
+exports.handler({
+  queryStringParameters: { text: 'test', gif: 'yes' }
+}).then(res => console.log(res.body))
